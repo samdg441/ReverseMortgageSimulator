485f57dbc612431dd202395595a9dc0ee5b62192
T-cordoba <tommy.cormo@gmail.com>
2024-08-15 23:23:58 -0500

    Defined initial Exceptions

---
+++ src/exceptions/exceptions.py
@@ -0,0 +1,32 @@
+class NegativeInterest(Exception):
+    pass
+
+
+class AboveMaxInterest(Exception):
+    pass
+
+
+class NegativePropertyValue(Exception):
+    pass
+
+
+class PropertyZeroValue(Exception):
+    pass
+
+
+class NegativeAge(Exception):
+    pass
+
+
+class AboveMaxAge(Exception):
+    pass
+
+
+class InvalidGender(Exception):
+    pass
+
+
+class InvalidMaritalStatus(Exception):
+    pass
+
+
