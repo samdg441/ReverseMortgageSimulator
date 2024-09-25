0d6e7fdc18dc061724ea9a5962f7b0f3c33e9bf2
T-cordoba <tommy.cormo@gmail.com>
2024-09-24 21:20:54 -0500

    Refactor GUI layout creation and organization
    This commit refactors the code responsible for creating the GUI layout. The `create_client_information_layout` and `create_property_information_layout` methods have been moved inside the `ReverseMortgageApp` class and renamed to `create_client_information_layout` and `create_property_information_layout`, respectively. The `create_inputs_layout` and `create_global_layout` methods have also been moved inside the `ReverseMortgageApp` class and renamed to `create_inputs_layout` and `create_global_layout`, respectively. This reorganization improves code readability and maintainability.
    
    The changes include:
    - Moving the layout creation methods inside the `ReverseMortgageApp` class
    - Renaming the layout creation methods to follow naming conventions
    - Updating method calls to reflect the new method names

--- src/GUI/gui.py
+++ src/GUI/gui.py
@@ -6,7 +6,6 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.spinner import Spinner
 from kivy.uix.popup import Popup
-
 import sys
 sys.path.append("src")
 
@@ -19,94 +18,93 @@ class EmptyInput(Exception):
 class ReverseMortgageApp(App):
     def build(self):
         """
-        Method responsible for creating the GUI.
+        Main method that initializes the application by creating the GUI layouts.
         """
-        def create_client_information_layout():
-            """
-            Method responsible for creating the client information layout.
-            """
-            layout = GridLayout(cols=2, spacing=10)
-
-            self.age_label = Label(text="Age *", size_hint=(0.5, 0.15))
-            layout.add_widget(self.age_label)
-            self.age_input = TextInput(multiline=False, size_hint=(0.5, 0.15), input_filter="int", text='0')
-            layout.add_widget(self.age_input)
-            self.age_input.bind(text=self.check_valid_age)
-
-            self.gender_label = Label(text="Gender *", size_hint=(0.5, 0.15))
-            layout.add_widget(self.gender_label)
-            self.gender_spinner = Spinner(text="Select", values=('M', 'F'), size_hint=(0.5, 0.15))
-            layout.add_widget(self.gender_spinner)
-            self.gender_spinner.bind(text=self.check_valid_spinner)
-
-            self.marital_status_label = Label(text="Marital Status *", size_hint=(0.5, 0.15))
-            layout.add_widget(self.marital_status_label)
-            self.marital_status_spinner = Spinner(text="Select", values=('Married', 'Single', 'Widowed', 'Divorced'), size_hint=(0.5, 0.15))
-            layout.add_widget(self.marital_status_spinner)
-            self.marital_status_spinner.bind(text=self.update_spouse_fields)
-            self.marital_status_spinner.bind(text=self.check_valid_spinner)
-
-            self.spouse_age_input = Label(text='None')
-            self.spouse_gender_spinner = Label(text='None')
-
-            self.is_married = False
-
-            return layout
+        self.client_layout = self.create_client_information_layout()
+        self.property_layout = self.create_property_information_layout()
+        self.inputs_layout = self.create_inputs_layout(self.client_layout, self.property_layout)
+        self.global_layout = self.create_global_layout(self.inputs_layout)
         
-
-        def create_property_information_layout():
-            """
-            Method responsible for creating the property information layout.
-            """
-            layout = GridLayout(cols=2)
-
-            self.property_value_label = Label(text="Home Value *", size_hint=(0.5, 0.1))
-            layout.add_widget(self.property_value_label)
-            self.property_value_input = TextInput(multiline=False, size_hint=(0.5, 0.1), input_filter="int", text='0')
-            layout.add_widget(self.property_value_input)
-
-            self.interest_rate_label = Label(text="Interest Rate *", size_hint=(0.5, 0.1))
-            layout.add_widget(self.interest_rate_label)
-            self.interest_rate_input = TextInput(multiline=False, size_hint=(0.5, 0.1), input_filter="float", text='0')
-            layout.add_widget(self.interest_rate_input)
-
-            return layout
-        
-        def create_inputs_layout(client_layout, property_layout):
-            """
-            Method responsible for creating the inputs layout.
-            """
-            layout = BoxLayout(orientation="horizontal", padding=15, spacing=10)
-
-            layout.add_widget(client_layout)
-            layout.add_widget(property_layout)
-
-            return layout
-
-        def create_global_layout(inputs_layout):
-            """
-            Method responsible for creating the global layout.
-            """
-            layout = GridLayout(rows=3, padding=15, spacing=10)
-
-            layout.add_widget(Label(text="Reverse Mortgage Calculator", size_hint=(1, 0.1), halign="center", font_family="Arial", font_size=30))
-
-            layout.add_widget(inputs_layout)
-
-            self.calc_button = Button(text="Calculate", size_hint=(0.5, 0.1))
-            layout.add_widget(self.calc_button)
-            self.calc_button.bind(on_press=self.calculate_reverse_mortgage)
-
-            return layout
-        
-        self.client_layout = create_client_information_layout()
-        self.property_layout = create_property_information_layout()
-        self.inputs_layout = create_inputs_layout(self.client_layout, self.property_layout)
-        self.global_layout = create_global_layout(self.inputs_layout)
-
         return self.global_layout
-    
+
+    def create_client_information_layout(self):
+        """
+        Creates and returns the layout for collecting client information.
+        """
+        layout = GridLayout(cols=2, spacing=10)
+
+        self.age_label = Label(text="Age *", size_hint=(0.5, 0.15))
+        layout.add_widget(self.age_label)
+        self.age_input = TextInput(multiline=False, size_hint=(0.5, 0.15), input_filter="int", text='0')
+        layout.add_widget(self.age_input)
+        self.age_input.bind(text=self.check_valid_age)
+
+        self.gender_label = Label(text="Gender *", size_hint=(0.5, 0.15))
+        layout.add_widget(self.gender_label)
+        self.gender_spinner = Spinner(text="Select", values=('M', 'F'), size_hint=(0.5, 0.15))
+        layout.add_widget(self.gender_spinner)
+        self.gender_spinner.bind(text=self.check_valid_spinner)
+
+        self.marital_status_label = Label(text="Marital Status *", size_hint=(0.5, 0.15))
+        layout.add_widget(self.marital_status_label)
+        self.marital_status_spinner = Spinner(text="Select", values=('Married', 'Single', 'Widowed', 'Divorced'), size_hint=(0.5, 0.15))
+        layout.add_widget(self.marital_status_spinner)
+        self.marital_status_spinner.bind(text=self.update_spouse_fields)
+        self.marital_status_spinner.bind(text=self.check_valid_spinner)
+
+        # Placeholder for spouse fields
+        self.spouse_age_input = Label(text='None')
+        self.spouse_gender_spinner = Label(text='None')
+        self.is_married = False
+
+        return layout
+
+    def create_property_information_layout(self):
+        """
+        Creates and returns the layout for collecting property information.
+        """
+        layout = GridLayout(cols=2)
+
+        self.property_value_label = Label(text="Home Value *", size_hint=(0.5, 0.1))
+        layout.add_widget(self.property_value_label)
+        self.property_value_input = TextInput(multiline=False, size_hint=(0.5, 0.1), input_filter="int", text='0')
+        layout.add_widget(self.property_value_input)
+
+        self.interest_rate_label = Label(text="Interest Rate *", size_hint=(0.5, 0.1))
+        layout.add_widget(self.interest_rate_label)
+        self.interest_rate_input = TextInput(multiline=False, size_hint=(0.5, 0.1), input_filter="float", text='0')
+        layout.add_widget(self.interest_rate_input)
+
+        return layout
+
+    def create_inputs_layout(self, client_layout, property_layout):
+        """
+        Combines the client and property information layouts into a horizontal layout.
+        """
+        layout = BoxLayout(orientation="horizontal", padding=15, spacing=10)
+        layout.add_widget(client_layout)
+        layout.add_widget(property_layout)
+        return layout
+
+    def create_global_layout(self, inputs_layout):
+        """
+        Creates the global layout, which includes the input forms and the calculation button.
+        """
+        layout = GridLayout(rows=3, padding=15, spacing=10)
+
+        layout.add_widget(Label(text="Reverse Mortgage Calculator", size_hint=(1, 0.1), halign="center", font_family="Arial", font_size=30))
+        layout.add_widget(inputs_layout)
+
+        self.calc_button = Button(text="Calculate", size_hint=(0.5, 0.1))
+        layout.add_widget(self.calc_button)
+        self.calc_button.bind(on_press=self.calculate_reverse_mortgage)
+
+        return layout
+
     def update_spouse_fields(self, sender, text):
+        """
+        Updates the form when the marital status is set to 'Married', showing additional fields.
+        """
         if text == 'Married' and not self.is_married:
             self.spouse_age_label = Label(text="Spouse's Age *", size_hint=(0.5, 0.15))
             self.client_layout.add_widget(self.spouse_age_label)
@@ -122,16 +120,17 @@ class ReverseMortgageApp(App):
             self.client_layout.add_widget(self.spouse_gender_spinner)
 
             self.is_married = True
-    
         elif text != 'Married' and self.is_married:
             self.client_layout.remove_widget(self.spouse_age_input)
             self.client_layout.remove_widget(self.spouse_age_label)
             self.client_layout.remove_widget(self.spouse_gender_label)
             self.client_layout.remove_widget(self.spouse_gender_spinner)
-
             self.is_married = False
 
     def check_valid_age(self, sender, text):
+        """
+        Validates the age input to ensure it's within a valid range (0-100).
+        """
         if sender is self.age_input:
             if (text != '' and text != '-') and (int(text) < 0 or int(text) > 100):
                 self.age_label.text = "Age *"
@@ -146,8 +145,11 @@ class ReverseMortgageApp(App):
                 self.spouse_age_label.text = "Spouse's Age *"
             else:
                 self.spouse_age_label.text = "Spouse's Age"
-        
+
     def check_valid_spinner(self, sender, text):
+        """
+        Checks that the Spinner values are correctly set (not 'Select').
+        """
         if text != 'Select' and sender is self.gender_spinner:
             self.gender_label.text = 'Gender'
         elif text != 'Select' and sender is self.spouse_gender_spinner:
@@ -156,6 +158,9 @@ class ReverseMortgageApp(App):
             self.marital_status_label.text = 'Marital Status'
 
     def calculate_reverse_mortgage(self, sender):
+        """
+        Calculates the reverse mortgage when the button is pressed and displays a result or error message.
+        """
         try:
             for layout in self.inputs_layout.children:
                 for child in layout.children:
@@ -169,7 +174,7 @@ class ReverseMortgageApp(App):
 
             reverse_mortgage = MonthlyPayment.ReverseMortgage(int(self.property_value_input.text), float(self.interest_rate_input.text), client)
 
-            popup = Popup(title = 'Reverse Mortgage Calculated Succesfully', content = Label(text = f'{reverse_mortgage}'), size_hint = (0.8, 0.4))
+            popup = Popup(title = 'Reverse Mortgage Calculated Successfully', content = Label(text = f'{reverse_mortgage}'), size_hint = (0.8, 0.4))
             popup.content.bind(on_touch_down=popup.dismiss)
             popup.open()
         except (MonthlyPayment.ClientException, MonthlyPayment.ReverseMortgageException, EmptyInput) as e:
