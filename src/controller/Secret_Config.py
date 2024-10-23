7012bc26970c9eed20fdb251afd8d69e227ade65
samdg441 <samuelgallego441@gmail.com>
2024-10-23 09:19:39 -0500

    final commit

--- src/controller/Secret_Config.py
+++
@@ -1,7 +0,0 @@
-# Do not expose your Neon credentials to the browser
-
-PGHOST='ep-small-rice-a5nl6w7c.us-east-2.aws.neon.tech'
-PGDATABASE='neondb'
-PGUSER='neondb_owner'
-PGPASSWORD='5RKBOqsPj3en'
-PGPORT = 5432
